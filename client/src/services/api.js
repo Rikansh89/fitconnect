f43243048384df263bde8c0aa00129af4c856749
RIKANSH THAKUR <160917027+Rikansh89@users.noreply.github.com>
2026-06-28 16:50:58 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({
+  baseURL: 'https://fitconnect-26og.onrender.com/api'
+});
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem('fitconnect-user'));
